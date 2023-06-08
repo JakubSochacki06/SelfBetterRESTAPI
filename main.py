ae2d31034c83d53260c931bd72abe6a9f04f71db
JakubSochacki06 <jsochacki03@gmail.com>
2023-06-08 17:51:21 +0200

    welcome page update

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import random
 from flask import Flask
 app = Flask(__name__)
 from quotes import quotes
+
+@app.get("/")
+def welcome_text():
+    return {"Text":"Welcome in SelfBetterAPI"}
 @app.get("/randomQuote")
 def get_random_quote():
     return quotes[random.randint(0,100)]
